03f88e5beb11e127d6bc7d9b731ae8ef4a4435d8
Nitin SINGH <nitinkumar.singh@f5.com>
2020-07-06 00:32:06 +0530

    adding goal to achive in this repo

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [outputText, setOutputText] = useState(
     "Open up App.js to start working on your app!"
   );
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Click to Change Text"
-        onPress={() => setOutputText("This text has changed!!!")}
-      />
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// add some styling to the view
