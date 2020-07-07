81aa31bcdc34c28035678b3dddb6446b856fbebb
Nitin SINGH <nitinkumar.singh@f5.com>
2020-07-07 23:54:39 +0530

    aligning element by flex box css

--- App.js
+++ App.js
@@ -8,14 +8,24 @@ export default function App() {
   );
   return (
     <View style={{ padding: 50 }}>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <TextInput
           placeholder="Course Goal"
-          style={{ borderBottomColor: "black", borderBottomWidth: 1 }}
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+            width: "80%",
+          }}
         />
         <Button title="ADD" />
       </View>
-      <View></View>
     </View>
   );
 }
