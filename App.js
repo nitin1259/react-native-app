b1717cbaae19f101ed16d31c8246202538ab6f11
Nitin SINGH <nitinkumar.singh@f5.com>
2020-07-08 00:22:37 +0530

    adding style and stylesheet object

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
@@ -7,27 +6,28 @@ export default function App() {
     "Open up App.js to start working on your app!"
   );
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%",
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: { padding: 50 },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+});
 
 // add some styling to the view
