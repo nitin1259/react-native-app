04503991452b2e5673ce7b323e80a499bb17f641
Nitin SINGH <nitinkumar.singh@f5.com>
2020-07-10 17:36:04 +0530

    extracting the card component

--- components/guessApp/screens/StartGameScreen.js
+++ components/guessApp/screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import Card from "../Card";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.titleText}>Start a New Game</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput placeholder="Enter a number" />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Start" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -33,15 +34,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    // shadow works only for iOS
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 5, // for android only
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
